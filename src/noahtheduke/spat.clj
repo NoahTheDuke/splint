258a09b8e1c90dfa155dc9195bd2162d3a0d1c8a
Noah Bogart <noah.bogart@hey.com>
2022-09-21 23:32:13 -0400

    Convert to macro form

--- src/noahtheduke/spat.clj
+++ src/noahtheduke/spat.clj
@@ -10,45 +10,39 @@
 
 (set! *warn-on-reflection* true)
 
-(def example-str
-  "(+ 1 2 :k)")
-
-(def example-input
-  (p/parse-string example-str))
-
-'{:tag :list,
-  :format-string "(%s)",
-  :wrap-length 2,
-  :seq-fn f
-  :children
-  ({:value +, :string-value "+"}
-   {:value nil, :string-value "nil"}
-   {:value true, :string-value "true"}
-   {:value 1, :string-value "1"}
-   {:lines ["a"]}
-   {:k :b, :namespaced? nil}
-   {:tag :quote,
-    :prefix "'",
-    :sym quote,
-    :children [{:value c, :string-value "c"}]}
-   {:tag :map,
-    :format-string "{%s}",
+#_'{:tag :list,
+    :format-string "(%s)",
     :wrap-length 2,
     :seq-fn f
     :children
-    ({:tag :list,
-      :format-string "(%s)",
+    ({:value +, :string-value "+"}
+     {:value nil, :string-value "nil"}
+     {:value true, :string-value "true"}
+     {:value 1, :string-value "1"}
+     {:lines ["a"]}
+     {:k :b, :namespaced? nil}
+     {:tag :quote,
+      :prefix "'",
+      :sym quote,
+      :children [{:value c, :string-value "c"}]}
+     {:tag :map,
+      :format-string "{%s}",
       :wrap-length 2,
       :seq-fn f
-      :children ({:value 1, :string-value "1"})}
-     {:value 2, :string-value "2"})}
-   {:tag :set,
-    :format-string "#{%s}",
-    :wrap-length 3,
-    :seq-fn f
-    :children ({:value 3, :string-value "3"})})}
+      :children
+      ({:tag :list,
+        :format-string "(%s)",
+        :wrap-length 2,
+        :seq-fn f
+        :children ({:value 1, :string-value "1"})}
+       {:value 2, :string-value "2"})}
+     {:tag :set,
+      :format-string "#{%s}",
+      :wrap-length 3,
+      :seq-fn f
+      :children ({:value 3, :string-value "3"})})}
 
-(defn read-dispatch [sexp]
+(defn read-dispatch [sexp _form]
   (cond
     ; literals
     (or (nil? sexp)
@@ -56,7 +50,10 @@
         (number? sexp)) :literal
     (keyword? sexp) :keyword
     (string? sexp) :string
-    (symbol? sexp) :symbol
+    (symbol? sexp) (cond
+                     (= '_ sexp) :any
+                     (= \% (first (name sexp))) :pred
+                     :else :symbol)
     ; reader macros
     (map? sexp) :map
     (set? sexp) :set
@@ -68,21 +65,18 @@
 
 (m/defmulti read-form read-dispatch)
 
-(m/defmethod read-form :default [sexp]
-  (prn :default (read-dispatch sexp)))
+(m/defmethod read-form :default [sexp form]
+  (prn :default (read-dispatch sexp form)))
 
-(m/defmethod read-form :literal [sexp]
-  (fn read-form-literal [form]
-    (= sexp (:value form))))
+(m/defmethod read-form :literal [sexp form]
+  `(= ~sexp (:value ~form)))
 
-(m/defmethod read-form :keyword [sexp]
-  (fn read-form-literal [form]
-    (= sexp (:k form))))
+(m/defmethod read-form :keyword [sexp form]
+  `(= ~sexp (:k ~form)))
 
-(m/defmethod read-form :string [sexp]
-  (fn read-form-string [form]
-    (when-let [lines (:lines form)]
-      (= (str/split-lines sexp) lines))))
+(m/defmethod read-form :string [sexp form]
+  `(when-let [lines# (:lines ~form)]
+     (= ~(str/split-lines sexp) lines#)))
 
 (defn get-val [form]
   (or (:value form)
@@ -90,58 +84,58 @@
       (some->> (:lines form) (str/join "\n"))
       (:children form)))
 
-(m/defmethod read-form :symbol [sexp]
-  (cond
-    (= '_ sexp) any?
-    (= \% (first (name sexp)))
-    (let [pred (symbol (subs (name sexp) 1))]
-      (fn read-form-pred [form]
-        ((resolve pred) (get-val form))))
-    :else (fn read-form-symbol [form]
-            (= sexp (:value form)))))
+(m/defmethod read-form :any [_sexp _form] nil)
 
-(m/defmethod read-form :map [sexp]
-  (let [{simple-keys true} (group-by #(= :literal %) (map read-dispatch sexp))
-        simple-sexp (select-keys sexp simple-keys)
-        simple-map (reduce-kv
-                     (fn [m k v]
-                       (assoc m k (read-form v)))
-                     {}
-                     simple-sexp)]
-    (assert (= (count simple-keys) (count sexp)) "todo complex keys")
-    (fn read-form-map [form]
-      (let [children (:children form)]
-        (and (= :map (:tag form))
-             (= (count simple-map) (count children))
-             (let [form-map (reduce
-                              (fn [m [k v]]
-                                (assoc m (get-val k) v))
-                              {}
-                              (partition 2 children))]
-               (reduce-kv
-                 (fn [_ k pred]
-                   (or (and (contains? form-map k)
-                            (pred (get form-map k)))
-                       (reduced false)))
-                 false
-                 simple-map)))))))
+(m/defmethod read-form :pred [sexp form]
+  ;; pred has to be unquoted and then quoted to keep it a simple keyword,
+  ;; not fully qualified, as it needs to be resolved in the calling namespace
+  ;; to allow for custom predicate functions
+  (let [pred (symbol (subs (name sexp) 1))]
+    `((resolve '~pred) (get-val ~form))))
 
-(m/defmethod read-form :list [sexp]
-  (let [preds (mapv read-form sexp)]
-    (fn read-form-list [form]
-      (and (= :list (:tag form))
-           (= (count preds) (count (:children form)))
-           (reduce
-             (fn [_ [pred child]]
-               (or (pred child)
-                   (reduced false)))
-             false
-             (map vector preds (:children form)))))))
+(m/defmethod read-form :symbol [sexp form]
+  `(= '~sexp (:value ~form)))
 
-(defn pattern [sexp]
-  (read-form sexp))
+(defn- read-form-seq [sexp form tag]
+  (let [children-form (gensym (str (name tag) "-form-"))
+        preds (keep-indexed
+                (fn [idx item]
+                  (read-form item `(nth ~children-form ~idx)))
+                sexp)]
+    `(and (= ~tag (:tag ~form))
+          (let [~children-form (:children ~form)]
+            (and (= ~(count sexp) (count ~children-form))
+                 ~@preds)))))
+
+(m/defmethod read-form :list [sexp form]
+  (read-form-seq sexp form :list))
+
+(m/defmethod read-form :vector [sexp form]
+  (read-form-seq sexp form :vector))
+
+(def simple? #{:literal :keyword :symbol})
+
+(m/defmethod read-form :map [sexp form]
+  (let [{simple-keys true} (->> (keys sexp)
+                                (group-by #(boolean (simple? (read-dispatch % form)))))
+        children-form (gensym "map-form-")
+        c-as-map (gensym "map-form-child-")
+        preds (keep (fn [k] (read-form (sexp k) `(~c-as-map ~k)))
+                    simple-keys)]
+    `(and (= :map (:tag ~form))
+          (let [~children-form (:children ~form)
+                ~c-as-map (->> ~children-form
+                               (partition 2)
+                               (map (fn [[k# v#]] [(get-val k#) v#]))
+                               (into {}))]
+            (and (<= ~(count simple-keys) (count ~c-as-map))
+                ~@preds)))))
+
+(defmacro pattern [sexp]
+  (let [form (gensym "form-")]
+    `(fn [~form] ~(read-form sexp form))))
 
 (comment
-  ((pattern '(+ 1 _ %keyword?)) example-input)
+  ((pattern {:a [+]}) (p/parse-string "{:a [+] [2] 3}"))
   (user/refresh-all)
   ,)

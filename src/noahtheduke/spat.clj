1eecb5c2049cb090e37aedf7b2f8a79baf2fce89
Noah Bogart <noah.bogart@hey.com>
2023-02-11 22:59:15 -0500

    Add rest of kibit rules

--- src/noahtheduke/spat.clj
+++ src/noahtheduke/spat.clj
@@ -232,16 +232,14 @@
   [rule-name & opts]
   (let [docs (when (string? (first opts)) (first opts))
         opts (if (string? (first opts)) (next opts) opts)
-        {:keys [pattern replace message replace-fn]} opts]
+        {:keys [pattern replace replace-fn]} opts]
     (assert (simple-symbol? rule-name) "defrule name cannot be namespaced")
-    (assert (and pattern message) "defrule requires a pattern and message")
     (assert (not (and replace replace-fn))
             "defrule cannot define both replace and replace-fn")
     `(def ~rule-name
        {:name ~(str rule-name)
         :docstring ~docs
         :pattern (pattern ~pattern)
-        :message '~message
         :replace (when ~replace
                    (fn ~(symbol (str rule-name "-replacer-fn"))
                      [form#]
@@ -251,52 +249,42 @@
 
 (defrule plus-x-1
   {:pattern '(+ ?x 1)
-   :message ""
    :replace '(inc ?x)})
 
 (defrule plus-1-x
   {:pattern '(+ 1 ?x)
-   :message ""
    :replace '(inc ?x)})
 
 (defrule minus-x-1
   {:pattern '(- ?x 1)
-   :message ""
    :replace '(dec ?x)})
 
 (defrule nested-muliply
   {:pattern '(* ?x (* &&. ?xs))
-   :message ""
    :replace '(* ?x &&. ?xs)})
 
 (defrule nested-addition
   {:pattern '(+ ?x (+ &&. ?xs))
-   :message ""
    :replace '(+ ?x &&. ?xs)})
 
 (defrule plus-0
   {:pattern '(+ ?x 0)
-   :message ""
    :replace '?x})
 
 (defrule minus-0
   {:pattern '(- ?x 0)
-   :message ""
    :replace '?x})
 
 (defrule multiply-by-1
   {:pattern '(* ?x 1)
-   :message ""
    :replace '?x})
 
 (defrule divide-by-1
   {:pattern '(/ ?x 1)
-   :message ""
    :replace '?x})
 
 (defrule multiply-by-0
   {:pattern '(* ?x 0)
-   :message ""
    :replace '0})
 
 (def math-rules
@@ -314,25 +302,21 @@
 (defrule str-to-string
   "(.toString) to (str)"
   {:pattern '(.toString ?x)
-   :message ""
    :replace '(str ?x)})
 
 (defrule str-apply-interpose
   "(apply str (interpose)) to (str/join)"
   {:pattern '(apply str (interpose ?x ?y))
-   :message ""
    :replace '(clojure.string/join ?x ?y)})
 
 (defrule str-apply-reverse
   "(apply str (reverse)) to (str/reverse)"
   {:pattern '(apply str (reverse ?x))
-   :message ""
    :replace '(clojure.string/reverse ?x)})
 
 (defrule str-apply-str
   "(apply str) to (str/join)"
   {:pattern '(apply str ?x)
-   :message ""
    :replace '(clojure.string/join ?x)}) 
 
 (def string-rules
@@ -344,37 +328,30 @@
 
 (defrule mapcat-apply-apply
   {:pattern '(apply concat (apply map ?x ?y))
-   :message ""
    :replace '(mapcat ?x ?y)})
 
 (defrule mapcat-concat-map
   {:pattern '(apply concat (map ?x . ?y))
-   :message ""
    :replace '(mapcat ?x . ?y)})
 
 (defrule filter-complement
   {:pattern '(filter (complement ?pred) ?coll)
-   :message ""
    :replace '(remove ?pred ?coll)})
 
 (defrule filter-seq
   {:pattern '(filter seq ?coll)
-   :message ""
    :replace '(remove empty? ?coll)})
 
 (defrule filter-fn*-not-pred
   {:pattern '(filter (fn* [?x] (not (?pred ?x))) ?coll)
-   :message ""
    :replace '(remove ?pred ?coll)})
 
 (defrule filter-fn-not-pred
   {:pattern '(filter (fn [?x] (not (?pred ?x))) ?coll)
-   :message ""
    :replace '(remove ?pred ?coll)})
 
 (defrule filter-vec-filter
   {:pattern '(vec (filter ?pred ?coll))
-   :message ""
    :replace '(filterv ?pred ?coll)})
 
 (def sequence-rules
@@ -388,22 +365,18 @@
 
 (defrule first-first
   {:pattern '(first (first ?coll))
-   :message ""
    :replace '(ffirst ?coll)})
 
 (defrule first-next
   {:pattern '(first (next ?coll))
-   :message ""
    :replace '(fnext ?coll)})
 
 (defrule next-next
   {:pattern '(next (next ?coll))
-   :message ""
    :replace '(nnext ?coll)})
 
 (defrule next-first
   {:pattern '(next (first ?coll))
-   :message ""
    :replace '(nfirst ?coll)})
 
 (def first-next-rules
@@ -414,12 +387,10 @@
 
 (defrule fn*-wrapper
   {:pattern '(fn* [?arg] (?fun ?arg))
-   :message ""
    :replace '?fun})
 
 (defrule fn-wrapper
   {:pattern '(fn [?arg] (?fun ?arg))
-   :message ""
    :replace '?fun})
 
 (def fn-rules
@@ -429,7 +400,6 @@
 (defrule thread-first-no-arg
   "(-> x) to x"
   {:pattern '(-> ?x)
-   :message ""
    :replace '?x})
 
 (defn symbol-or-keyword-or-list? [sexp]
@@ -442,7 +412,6 @@
   "(-> x y) to (y x)
   (-> x (y)) to (y x)"
   {:pattern '(-> ?arg %symbol-or-keyword-or-list?%-?form)
-   :message ""
    :replace-fn (fn [{:syms [?arg ?form]}]
                  (if (list? ?form)
                    (list* (first ?form) ?arg (rest ?form))
@@ -451,14 +420,12 @@
 (defrule thread-last-no-arg
   "(->> x) to x"
   {:pattern '(->> ?x)
-   :message ""
    :replace '?x})
 
 (defrule thread-last-1-arg
   "(->> x y) to (y x)
   (->> x (y)) to (y x)"
   {:pattern '(->> ?arg %symbol-or-keyword-or-list?%-?form)
-   :message ""
    :replace-fn (fn [{:syms [?arg ?form]}]
                  (if (list? ?form)
                    (list* (concat ?form [?arg]))
@@ -472,18 +439,245 @@
 
 (defrule not-some-pred
   {:pattern '(not (some ?pred ?coll))
-   :message ""
    :replace '(not-any? ?pred ?coll)})
 
 (defrule with-meta-f-meta
   {:pattern '(with-meta ?x (?f (meta ?x) &&. ?args))
-   :message ""
    :replace-fn (fn [{:syms [?x ?f ?args]}]
                  (list* 'vary-meta ?x ?f ?args))})
 
 (def misc-rules
   [not-some-pred
    with-meta-f-meta])
+
+;;vector
+(defrule conj-vec
+  {:pattern '(conj [] &&. ?x)
+   :replace '(vector &&. ?x)})
+
+(defrule into-vec
+  {:pattern '(into [] ?coll)
+   :replace '(vec ?coll)})
+
+(defrule assoc-assoc-key-coll
+  {:pattern '(assoc ?coll ?key0 (assoc (?key0 ?coll) ?key1 ?val))
+   :replace '(assoc-in ?coll [?key0 ?key1] ?val)})
+
+(defrule assoc-assoc-coll-key
+  {:pattern '(assoc ?coll ?key0 (assoc (?coll ?key0) ?key1 ?val))
+   :replace '(assoc-in ?coll [?key0 ?key1] ?val)})
+
+(defrule assoc-assoc-get
+  {:pattern '(assoc ?coll ?key0 (assoc (get ?coll ?key0) ?key1 ?val))
+   :replace '(assoc-in ?coll [?key0 ?key1] ?val)})
+
+(defrule assoc-fn-key-coll
+  {:pattern '(assoc ?coll ?key (?fn (?key ?coll) &&. ?args))
+   :replace '(update-in ?coll [?key] ?fn &&. ?args)})
+
+(defrule assoc-fn-coll-key
+  {:pattern '(assoc ?coll ?key (?fn (?coll ?key) &&. ?args))
+   :replace '(update-in ?coll [?key] ?fn &&. ?args)})
+
+(defrule assoc-fn-get
+  {:pattern '(assoc ?coll ?key (?fn (get ?coll ?key) &&. ?args))
+   :replace '(update-in ?coll [?key] ?fn &&. ?args)})
+
+(defrule update-in-assoc
+  {:pattern '(update-in ?coll ?keys assoc ?val)
+   :replace '(assoc-in ?coll ?keys ?val)})
+
+;; empty?
+(defrule not-empty?
+  {:pattern '(not (empty? ?x))
+   :replace '(seq ?x)})
+
+(defrule when-not-empty?
+  {:pattern '(when-not (empty? ?x) &&. ?y)
+   :replace '(when (seq ?x) &&. ?y)})
+
+;; set
+(defrule into-set
+  {:pattern '(into #{} ?coll)
+   :replace '(set ?coll)})
+
+(defrule take-repeatedly
+  {:pattern '(take ?n (repeatedly ?coll))
+   :replace '(repeatedly ?n ?coll)})
+
+(defrule dorun-map
+  {:pattern '(dorun (map ?fn ?coll))
+   :replace '(run! ?fn ?coll)})
+
+(def coll-rules
+  [conj-vec
+   into-vec
+   assoc-assoc-key-coll
+   assoc-assoc-coll-key
+   assoc-assoc-get
+   assoc-fn-key-coll
+   assoc-fn-coll-key
+   assoc-fn-get
+   update-in-assoc
+   not-empty?
+   when-not-empty?
+   into-set
+   take-repeatedly
+   dorun-map])
+
+(defrule if-else-nil
+  {:pattern '(if ?x ?y nil)
+   :replace '(when ?x ?y)})
+
+(defrule if-nil-else
+  {:pattern '(if ?x nil ?y)
+   :replace '(when-not ?x ?y)})
+
+(defrule if-then-do
+  {:pattern '(if ?x (do &&. ?y))
+   :replace '(when ?x &&. ?y)})
+
+(defrule if-not-x-y-x
+  {:pattern '(if (not ?x) ?y ?z)
+   :replace '(if-not ?x ?y ?z)})
+
+(defrule if-x-x-y
+  {:pattern '(if ?x ?x ?y)
+   :replace '(or ?x ?y)})
+
+(defrule when-not-x-y
+  {:pattern '(when (not ?x) &&. ?y)
+   :replace '(when-not ?x &&. ?y)})
+
+(defrule do-x
+  {:pattern '(do ?x)
+   :replace '?x})
+
+(defrule if-let-else-nil
+  {:pattern '(if-let ?binding ?expr nil)
+   :replace '(when-let ?binding ?expr)})
+
+(defrule when-do
+  {:pattern '(when ?x (do &&. ?y))
+   :replace '(when ?x &&. ?y)})
+
+(defrule when-not-do
+  {:pattern '(when-not ?x (do &&. ?y))
+   :replace '(when-not ?x &&. ?y)})
+
+(defrule if-not-do
+  {:pattern '(if-not ?x (do &&. ?y))
+   :replace '(when-not ?x &&. ?y)})
+
+(defrule if-not-not
+  {:pattern '(if-not (not ?x) ?y ?z)
+   :replace '(if ?x ?y ?z)})
+
+(defrule when-not-not
+  {:pattern '(when-not (not ?x) &&. ?y)
+   :replace '(when ?x &&. ?y)})
+
+(defrule loop-empty-when
+  {:pattern '(loop [] (when ?test &&. ?exprs (recur)))
+   :replace '(while ?test &&. ?exprs)})
+
+(defrule let-do
+  {:pattern '(let ?binding (do &&. ?exprs))
+   :replace '(let ?binding &&. ?exprs)})
+
+(defrule loop-do
+  {:pattern '(loop ?binding (do &&. ?exprs))
+   :replace '(loop ?binding &&. ?exprs)})
+
+(def control-flow-rules
+  [if-else-nil
+   if-nil-else
+   if-then-do
+   if-not-x-y-x
+   if-x-x-y
+   when-not-x-y
+   do-x
+   if-let-else-nil
+   when-do
+   when-not-do
+   if-not-do
+   if-not-not
+   when-not-not
+   loop-empty-when
+   let-do
+   loop-do])
+
+(defrule not-eq
+  {:pattern '(not (= &&. ?args))
+   :replace '(not= &&. ?args)})
+
+(defrule eq-0-x
+  {:pattern '(= 0 ?x)
+   :replace '(zero? ?x)})
+
+(defrule eq-x-0
+  {:pattern '(= ?x 0)
+   :replace '(zero? ?x)})
+
+(defrule eqeq-0-x
+  {:pattern '(== 0 ?x)
+   :replace '(zero? ?x)})
+
+(defrule eqeq-x-0
+  {:pattern '(== ?x 0)
+   :replace '(zero? ?x)})
+
+(defrule lt-0-x
+  {:pattern '(< 0 ?x)
+   :replace '(pos? ?x)})
+
+(defrule gt-x-0
+  {:pattern '(> ?x 0)
+   :replace '(pos? ?x)})
+
+(defrule lt-x-0
+  {:pattern '(< ?x 0)
+   :replace '(neg? ?x)})
+
+(defrule gt-0-x
+  {:pattern '(> 0 ?x)
+   :replace '(neg? ?x)})
+
+(defrule eq-true
+  {:pattern '(= true ?x)
+   :replace '(true? ?x)})
+
+(defrule eq-false
+  {:pattern '(= false ?x)
+   :replace '(false? ?x)})
+
+(defrule eq-x-nil
+  {:pattern '(= ?x nil)
+   :replace '(nil? ?x)})
+
+(defrule eq-nil-x
+  {:pattern '(= nil ?x)
+   :replace '(nil? ?x)})
+
+(defrule not-nil?
+  {:pattern '(not (nil? ?x))
+   :replace '(some? ?x)})
+
+(def equality-rules
+  [not-eq
+   eq-0-x
+   eq-x-0
+   eqeq-0-x
+   eqeq-x-0
+   lt-0-x
+   lt-x-0
+   gt-0-x
+   gt-x-0
+   eq-true
+   eq-false
+   eq-x-nil
+   eq-nil-x
+   not-nil?])
 
 (def all-rules
   (vec (concat string-rules
@@ -492,7 +686,10 @@
                fn-rules
                threading-rules
                misc-rules
-               math-rules)))
+               math-rules
+               coll-rules
+               control-flow-rules
+               equality-rules)))
 
 (defn check-rule [rule form]
   (let [pattern (:pattern rule)

a5ae86b1af06013e0c715b06fceb55e568ee7212
Noah Bogart <noah.bogart@hey.com>
2023-03-08 22:45:48 -0500

    Add pattern tests

--- test/noahtheduke/spat/pattern_test.clj
+++ test/noahtheduke/spat/pattern_test.clj
@@ -4,18 +4,40 @@
 
 (ns noahtheduke.spat.pattern-test
   (:require
-    [expectations.clojure.test :refer [defexpect expect]]
+    [expectations.clojure.test :refer [defexpect expect from-each]]
     [noahtheduke.splint.parser :refer [parse-string]]
     [noahtheduke.spat.pattern :refer [pattern]]))
 
-(defexpect multiple-rest-body
+(defexpect match-any-test
+  (expect {}
+    (from-each [input '[a 'a :a "a" 1 true nil
+                        (1 2 3)
+                        {1 2}
+                        #{1 2 3}
+                        [1 2 3]]]
+      ((pattern '(1 2 _)) (list 1 2 input)))))
+
+(defexpect predicate-test
+  (expect {}
+    ((pattern '(1 2 %symbol?)) '(1 2 a)))
+  (expect nil?
+    (from-each [input '['a :a "a" 1 true nil
+                        (1 2 3)
+                        {1 2}
+                        #{1 2 3}
+                        [1 2 3]]]
+      ((pattern '(1 2 %symbol?)) (list 1 2 input))))
+  (expect '{?a a}
+    ((pattern '(1 2 %symbol?%-?a)) '(1 2 a))))
+
+(defexpect multiple-rest-body-test
   '{?test (= 1 1)
     ?exprs ((prn 1) (prn 2))
     ?foo (foo bar)}
   ((pattern '(when ?test &&. ?exprs ?foo (recur)))
    (parse-string "(when (= 1 1) (prn 1) (prn 2) (foo bar) (recur))")))
 
-(defexpect quote-in-pattern
+(defexpect quote-in-pattern-test
   '{}
   ((pattern '(a b 'c))
    (parse-string "(a b 'c)")))

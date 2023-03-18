2f33042b2cb72da32ab6c5dbc202f53c055be21e
Noah Bogart <noah.bogart@hey.com>
2023-03-17 20:46:23 -0400

    Use git edamame until uneval branch is merged

--- src/noahtheduke/spat/parser.clj
+++ src/noahtheduke/spat/parser.clj
@@ -4,7 +4,6 @@
 
 (ns noahtheduke.spat.parser
   (:require
-    [clojure.string :as str]
     [edamame.core :as e]
     [edamame.impl.read-fn :as read-fn]
     [noahtheduke.spat.edamame-ext]))
@@ -15,15 +14,24 @@
 
 (set! *warn-on-reflection* true)
 
-(def base-edamame-opts
+(defn make-edamame-opts []
   {:all true
    :row-key :line
    :col-key :column
    :end-location true
-   :location? seq?
    :features #{:cljs}
    :read-cond :preserve
    :readers (fn [r] (fn [v] (list (if (namespace r) r (symbol "splint-auto" (name r))) v)))
+   :auto-resolve-ns true
+   :auto-resolve (fn [k] (if (= :current k) "splint-auto_" (str "splint-auto_" (name k))))
+   :uneval (fn [{:keys [uneval next]}]
+             (cond
+               (identical? uneval :splint/disable)
+               (with-meta next {:splint/disable true})
+               (seq (:splint/disable uneval))
+               (with-meta next {:splint/disable (seq (:splint/disable uneval))})
+               :else
+               next))
    ; All reader macros should be a splint-specific symbol wrapping the expression
    :dispatch {; @x
               \@ (fn [expr] (list 'splint/deref expr))
@@ -45,52 +53,5 @@
                   ; #".*"
                   \" (fn [expr] (list 'splint/re-pattern expr))}}})
 
-(defn iobj?
-  "Adapted from edamame.impl.parser."
-  [x]
-  (instance? clojure.lang.IObj x))
-
-(defn make-postprocess
-  "Adapted from edamame.impl.parser."
-  [ns-state]
-  (fn postprocess [{:keys [obj loc]}]
-    (when-let [ns-parsed (when (and (seq? obj)
-                                    (= 'ns (first obj)))
-                           (try (e/parse-ns-form obj)
-                                (catch Exception _ nil)))]
-      (reset! ns-state (assoc (:aliases ns-parsed) :current (:name ns-parsed))))
-    ;; Gotta apply location data here as using `:postprocess` skips automatic
-    ;; location data
-    (if (iobj? obj) (vary-meta obj merge loc) obj)))
-
-(defn make-edamame-opts
-  "Because of boxing, I need to perform `:auto-resolve-ns` myself. I can't
-  access the `:ns-state` on edamame's `ctx`, so instead make a new `ns-state`
-  each invocation of [[parse-string]] and [[parse-string-all]] to allow for
-  parallel parsing."
-  []
-  (let [ns-state (atom {})]
-    (-> base-edamame-opts
-        (assoc :auto-resolve
-               (fn [k] (if-let [resolved-ns (@ns-state k)]
-                         resolved-ns
-                         (if (= :current k)
-                           "splint-auto_"
-                           (str "splint-auto_" (name k))))))
-        (assoc :postprocess (make-postprocess ns-state)))))
-
-(defn convert-splint-ignore [s]
-  (str/replace s #"#_(\{)?:splint/disable" "^$1:splint/disable"))
-
-(comment
-  (convert-splint-ignore "asdf #_{:splint/disable [lint]} (+ 1 x)"))
-
-(defn parse-string [s]
-  (-> s
-      (convert-splint-ignore)
-      (e/parse-string (make-edamame-opts))))
-
-(defn parse-string-all [s]
-  (-> s
-      (convert-splint-ignore)
-      (e/parse-string-all (make-edamame-opts))))
+(defn parse-string [s] (e/parse-string s (make-edamame-opts)))
+(defn parse-string-all [s] (e/parse-string-all s (make-edamame-opts)))

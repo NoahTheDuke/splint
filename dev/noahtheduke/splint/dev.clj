c32689a2e79a2995ccb437c3f4ed63a050ebf97f
Noah Bogart <noah.bogart@hey.com>
2023-05-26 22:08:01 -0400

    Add tufte to user.clj, enable all dev rules on load

--- dev/noahtheduke/splint/dev.clj
+++ dev/noahtheduke/splint/dev.clj
@@ -6,20 +6,36 @@
   (:require
     [clojure.java.io :as io]
     [nextjournal.beholder :as beholder]
-    [noahtheduke.splint.config :as config]))
+    [noahtheduke.splint.config :as config]
+    [noahtheduke.splint.rules :refer [global-rules]]
+    [noahtheduke.splint]
+    [taoensso.tufte :as tufte]))
+
+(set! *warn-on-reflection* true)
 
 (doseq [dev-rule (file-seq (io/file "dev" "noahtheduke" "splint" "rules" "dev"))
-        :when (.isFile dev-rule)]
+        :when (.isFile ^java.io.File dev-rule)]
   (load-file (str dev-rule)))
 
-(def default-config (atom (config/read-default-config)))
+(defn build-default-config []
+  (let [dev-rules (->> (keys @global-rules)
+                       (filter #(= "dev" (namespace %)))
+                       (map (fn [r] (clojure.lang.MapEntry. r {:enabled true})))
+                       (into {}))
+        default-rules (config/read-default-config)]
+    (merge dev-rules default-rules)))
+
+(def default-config (atom (build-default-config)))
 
 (def watcher
   (beholder/watch
     (fn [action]
       (when (#{:create :modify} (:type action))
-        (reset! default-config (config/read-default-config))))
+        (reset! default-config (build-default-config))))
     "resources"))
+
+
+(tufte/add-basic-println-handler! {})
 
 (comment
   (beholder/stop watcher))

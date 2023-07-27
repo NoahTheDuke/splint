e9e05d193a8b003c051b07d02b2689a27df009c9
Noah Bogart <noah.bogart@hey.com>
2023-07-26 20:43:26 -0400

    Add tests to lint/into-literal

--- test/noahtheduke/splint/rules/lint/into_literal_test.clj
+++ test/noahtheduke/splint/rules/lint/into_literal_test.clj
@@ -5,14 +5,44 @@
 (ns noahtheduke.splint.rules.lint.into-literal-test
   (:require
     [expectations.clojure.test :refer [defexpect]]
-    [noahtheduke.splint.test-helpers :refer [expect-match]]))
+    [noahtheduke.splint.test-helpers :refer [expect-match single-rule-config]]))
+
+(set! *warn-on-reflection* true)
+
+(defn config [] (single-rule-config 'lint/into-literal))
 
 (defexpect into-vec-test
   (expect-match
     '[{:alt (vec coll)}]
-    "(into [] coll)"))
+    "(into [] coll)"
+    (config))
+  (expect-match
+    '[{:alt (vec (range 100))}]
+    "(into [] (range 100))"
+    (config))
+  (expect-match
+    nil
+    "(into [] xf coll)"
+    (config))
+  (expect-match
+    nil
+    "(into [1 2] coll)"
+    (config)))
 
 (defexpect into-set-test
   (expect-match
     '[{:alt (set coll)}]
-    "(into #{} coll)"))
+    "(into #{} coll)"
+    (config))
+  (expect-match
+    '[{:alt (set (range 100))}]
+    "(into #{} (range 100))"
+    (config))
+  (expect-match
+    nil
+    "(into #{} xf coll)"
+    (config))
+  (expect-match
+    nil
+    "(into #{1 2} coll)"
+    (config)))

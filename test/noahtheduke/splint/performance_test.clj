fed481b0cf25a0f647d306da0d37366334e6a50b
Noah Bogart <noah.bogart@hey.com>
2024-05-08 21:29:31 -0400

    Update performance gutchecks

--- test/noahtheduke/splint/performance_test.clj
+++ test/noahtheduke/splint/performance_test.clj
@@ -19,16 +19,18 @@
   (let [clj-kondo (gl/procure "https://github.com/clj-kondo/clj-kondo.git" 'clj-kondo/clj-kondo "v2023.05.26")
         analyzer (io/file clj-kondo "src" "clj_kondo" "impl" "analyzer.clj")
         original-rules @global-rules]
-    ; v1.3 -> v1.8
-    ; (swap! global-rules update-vals #(assoc % :pattern (constantly nil)))
-    ; v1.9+
-    (swap! global-rules update :rules update-vals #(assoc % :pattern (constantly nil)))
-    ; (prn (count (clojure.string/split-lines (slurp analyzer))))
-    (with-redefs [noahtheduke.splint.config/load-config all-enabled-config]
-      #_:clj-kondo/ignore
-      (user/quick-bench
-        (with-out-str (run ["--quiet" "--no-parallel" (str analyzer)]))))
-    (reset! global-rules original-rules)
+    (try
+      ; v1.3 -> v1.8
+      ; (swap! global-rules update-vals #(assoc % :pattern (constantly nil)))
+      ; v1.9+
+      (swap! global-rules update :rules update-vals #(assoc % :pattern (constantly nil)))
+      (with-redefs [noahtheduke.splint.config/find-local-config
+                    (constantly all-enabled-config)]
+        #_:clj-kondo/ignore
+        (user/quick-bench
+          (with-out-str (run ["--quiet" "--no-parallel" (str analyzer)]))))
+      (finally
+        (reset! global-rules original-rules)))
     nil))
 
 (comment
@@ -95,3 +97,52 @@
 ;    Execution time lower quantile : 58.258922 ms ( 2.5%)
 ;    Execution time upper quantile : 62.431311 ms (97.5%)
 ;                    Overhead used : 7.220732 ns
+
+; v1.10.1
+; Evaluation count : 12 in 6 samples of 2 calls.
+;              Execution time mean : 58.221179 ms
+;     Execution time std-deviation : 2.562421 ms
+;    Execution time lower quantile : 55.682036 ms ( 2.5%)
+;    Execution time upper quantile : 61.124568 ms (97.5%)
+;                    Overhead used : 6.307295 ns
+
+; v1.11
+; Evaluation count : 12 in 6 samples of 2 calls.
+;              Execution time mean : 70.876517 ms
+;     Execution time std-deviation : 13.306443 ms
+;    Execution time lower quantile : 57.830551 ms ( 2.5%)
+;    Execution time upper quantile : 87.860982 ms (97.5%)
+;                    Overhead used : 6.295733 ns
+
+; v1.12
+; Evaluation count : 12 in 6 samples of 2 calls.
+;              Execution time mean : 83.102353 ms
+;     Execution time std-deviation : 9.324730 ms
+;    Execution time lower quantile : 73.872179 ms ( 2.5%)
+;    Execution time upper quantile : 93.105615 ms (97.5%)
+;                    Overhead used : 6.303118 ns
+
+; v1.13
+; Evaluation count : 12 in 6 samples of 2 calls.
+;              Execution time mean : 58.912171 ms
+;     Execution time std-deviation : 4.244262 ms
+;    Execution time lower quantile : 55.235069 ms ( 2.5%)
+;    Execution time upper quantile : 65.230776 ms (97.5%)
+;                    Overhead used : 6.287432 ns
+
+; v1.14.0
+; Evaluation count : 6 in 6 samples of 1 calls.
+;              Execution time mean : 90.747193 ms
+;     Execution time std-deviation : 21.460951 ms
+;    Execution time lower quantile : 73.026052 ms ( 2.5%)
+;    Execution time upper quantile : 116.331701 ms (97.5%)
+;                    Overhead used : 6.257130 ns
+
+; v1.15.1
+; Evaluation count : 12 in 6 samples of 2 calls.
+;              Execution time mean : 82.179354 ms
+;     Execution time std-deviation : 1.802063 ms
+;    Execution time lower quantile : 80.523114 ms ( 2.5%)
+;    Execution time upper quantile : 84.252506 ms (97.5%)
+;                    Overhead used : 6.331092 ns
+; nil

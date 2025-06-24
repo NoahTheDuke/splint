ea57d3a60520bc39dd701f3b6bc3b81e61e6e2db
Noah Bogart <noah.bogart@hey.com>
2025-06-24 11:11:18 -0400

    Move integrations tests to dedicated folder

--- test/noahtheduke/splint/integrations/netrunner_test.clj
+++ test/noahtheduke/splint/integrations/netrunner_test.clj
@@ -2,7 +2,7 @@
 ; License, v. 2.0. If a copy of the MPL was not distributed with this
 ; file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-(ns noahtheduke.splint.netrunner-test
+(ns noahtheduke.splint.integrations.netrunner-test
   (:require
    [clojure.tools.gitlibs :as gl]
    [lazytest.core :refer [defdescribe expect it]]
--- test/noahtheduke/splint/integrations/re_frame_test.clj
+++ test/noahtheduke/splint/integrations/re_frame_test.clj
@@ -2,7 +2,7 @@
 ; License, v. 2.0. If a copy of the MPL was not distributed with this
 ; file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-(ns noahtheduke.splint.re-frame-test
+(ns noahtheduke.splint.integrations.re-frame-test
   (:require
    [clojure.tools.gitlibs :as gl]
    [lazytest.core :refer [defdescribe expect it]]
